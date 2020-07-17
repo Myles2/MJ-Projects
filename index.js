526f3e003a16d3476c5f20f9cbf847b91b92beea
Myles2 <winnerkd288@gmail.com>
2020-07-17 05:21:08 -0400

    Added images locally for background, test code for skill library

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const spot = document.getElementById("spot");
 const spotName = document.getElementById("spotName");
+const single_skill = document.getElementById("single_skill");
+const skill = document.getElementById("skillName");
 const skills = ["icon-html5-alt", "icon-css3-alt", "icon-javascript-alt", "icon-jquery", "icon-sass", "icon-reactjs", "icon-git", "icon-cplusplus", "icon-python"]
 const skillName = ["HTML5", "CSS3", "JavaScript", "jQuery", "Sass", "React.js", "Git", "C++", "Python"]
 let count = 0;
@@ -14,4 +16,12 @@ setInterval(function() {
     count = 0;
   }
 }, 3000);
+
+/*
+Adding icons to a group display
+
+for (i = 0; i < skills.length; i++) {
+  document.getElementById('skills')
+}
+*/
 
